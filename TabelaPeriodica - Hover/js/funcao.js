c361f5bb6052c948bb5e21ffc8ac72975987d61a
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2023-12-10 23:34:11 -0300

    Tabela Periodica - CSS Grid Layout
    finish project

---
+++ TabelaPeriodica - Hover/js/funcao.js
@@ -0,0 +1,154 @@
+function mudaElemento(){
+    document.getElementById("modals").src = "imagens/Hidrogenio.png"; 
+    
+}	
+
+function mudaElemento1(){
+    document.getElementById("modals").src = "imagens/Helio.png"; 
+    
+}	
+
+function mudaElemento2(){
+    document.getElementById("modals").src = "imagens/Litio.png"; 
+    
+}	
+
+function mudaElemento3(){
+    document.getElementById("modals").src = "imagens/Berilio.png"; 
+    
+}	
+
+function mudaElemento4(){
+    document.getElementById("modals").src = "imagens/Boro.png"; 
+    
+}	
+
+function mudaElemento5(){
+    document.getElementById("modals").src = "imagens/Magnesio.png"; 
+    
+}	
+
+function mudaElemento6(){
+    document.getElementById("modals").src = "imagens/Sodio.png"; 
+    
+}	
+
+function mudaElemento7(){
+    document.getElementById("modals").src = "imagens/Carbono.png"; 
+    
+}	
+
+function mudaElemento8(){
+    document.getElementById("modals").src = "imagens/Nitrogenio.png"; 
+    
+}	
+
+function mudaElemento9(){
+    document.getElementById("modals").src = "imagens/Oxigenio.png"; 
+    
+}	
+
+function mudaElemento10(){
+    document.getElementById("modals").src = "imagens/Fluor.png"; 
+    
+}	
+
+function mudaElemento11(){
+    document.getElementById("modals").src = "imagens/Neonio.png"; 
+    
+}	
+
+function mudaElemento12(){
+    document.getElementById("modals").src = "imagens/Potassio.png"; 
+    
+}	
+
+function mudaElemento13(){
+    document.getElementById("modals").src = "imagens/Calcio.png"; 
+    
+}	
+
+function mudaElemento14(){
+    document.getElementById("modals").src = "imagens/Aluminio.png"; 
+    
+}	
+
+function mudaElemento15(){
+    document.getElementById("modals").src = "imagens/Silicio.png"; 
+    
+}	
+
+function mudaElemento16(){
+    document.getElementById("modals").src = "imagens/Fosforo.png"; 
+    
+}	
+
+function mudaElemento17(){
+    document.getElementById("modals").src = "imagens/Enxofre.png"; 
+    
+}	
+
+function mudaElemento18(){
+    document.getElementById("modals").src = "imagens/Cloro.png"; 
+    
+}	
+
+function mudaElemento19(){
+    document.getElementById("modals").src = "imagens/Argonio.png"; 
+    
+}	
+
+function mudaElemento20(){
+    document.getElementById("modals").src = "imagens/Galio.png"; 
+    
+}	
+
+function mudaElemento21(){
+    document.getElementById("modals").src = "imagens/Zinco.png"; 
+    
+}
+
+function mudaElemento22(){
+    document.getElementById("modals").src = "imagens/Cobre.png"; 
+    
+}	
+
+function mudaElemento23(){
+    document.getElementById("modals").src = "imagens/Niquel.png"; 
+    
+}	
+
+function mudaElemento24(){
+    document.getElementById("modals").src = "imagens/Cobalto.png"; 
+    
+}	
+
+function mudaElemento25(){
+    document.getElementById("modals").src = "imagens/Ferro.png"; 
+    
+}	
+
+function mudaElemento26(){
+    document.getElementById("modals").src = "imagens/Manganes.png"; 
+    
+}	
+
+function mudaElemento27(){
+    document.getElementById("modals").src = "imagens/Cromo.png"; 
+    
+}	
+
+function mudaElemento28(){
+    document.getElementById("modals").src = "imagens/Vanadio.png"; 
+    
+}
+
+function mudaElemento29(){
+    document.getElementById("modals").src = "imagens/Titanio.png"; 
+    
+}
+
+function mudaElemento30(){
+    document.getElementById("modals").src = "imagens/Escandio.png"; 
+    
+}
---
+++ TabelaPeriodica - JS/js/funcao.js
@@ -0,0 +1,39 @@
+function mudaElemento(){
+    document.getElementById("modals").src = "imagens/Hidrogenio.png"; 
+    
+}	
+
+function mudaElemento1(){
+    document.getElementById("modals").src = "imagens/Helio.png"; 
+    
+}	
+
+function mudaElemento2(){
+    document.getElementById("modals").src = "imagens/Litio.png"; 
+    
+}	
+
+function mudaElemento3(){
+    document.getElementById("modals").src = "imagens/Berilio.png"; 
+    
+}	
+
+function mudaElemento4(){
+    document.getElementById("modals").src = "imagens/Boro.png"; 
+    
+}	
+
+function mudaElemento5(){
+    document.getElementById("modals").src = "imagens/Magnesio.png"; 
+    
+}	
+
+function mudaElemento6(){
+    document.getElementById("modals").src = "imagens/Sodio.png"; 
+    
+}	
+
+function mudaElemento7(){
+    document.getElementById("modals").src = "imagens/Carbono.png"; 
+    
+}	
