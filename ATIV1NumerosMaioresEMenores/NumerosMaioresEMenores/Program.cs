e04c5407e512c449374d5c57d0fd2e80eec02ae0
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-12 11:57:05 -0300

    Exercicio C#

---
+++ ATIV1NumerosMaioresEMenores/NumerosMaioresEMenores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumerosMaioresEMenores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Informe o primeiro número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número !");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if(n1 > n2)
+            {
+                Console.WriteLine("O maior número é o :" + n1);
+            }
+            else
+            {
+                Console.WriteLine("O maior número é o :" + n2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
