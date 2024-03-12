8b93dec36d47167ede838cc0a52756a0360b98ef
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-12 11:58:28 -0300

    Exercicio C#

---
+++ Atividade1- NumerosInteiros/Atividade1- NumerosInteiros/Atividade1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Atividade1__NumerosInteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+
+            Console.WriteLine("Olá, informe o primeiro número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número !");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o terceiro número !");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("O maior número é o :" + n1);
+            }
+            else
+            {
+                if (n2 > n3)
+                {
+                    Console.WriteLine("O maior número é o :" + n2);
+                }
+                else
+                {
+                    Console.WriteLine("O maior número é o :" + n3);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
