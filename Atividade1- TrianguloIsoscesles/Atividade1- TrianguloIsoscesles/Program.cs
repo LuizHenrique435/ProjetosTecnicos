7cf843dbc6ac3f8da82df3681cf8e054d206a0ee
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-12 12:09:34 -0300

    Exercicio C#

---
+++ Atividade1- TrianguloIsoscesles/Atividade1- TrianguloIsoscesles/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Atividade1__TrianguloIsoscesles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+
+           Console.WriteLine("Olá informe a primeira medida (lado 1)! ");
+            A = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Informe a segunda medida (lado 2)");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a terceira medida (lado 3)");
+            C = Convert.ToInt32 (Console.ReadLine());
+
+            if (A == B)
+            {
+                if (B == C) Console.WriteLine("Equilátero");
+                if (B != C) Console.WriteLine("Isósceles");
+            }
+            if (A != B)
+            {
+                if (B != C)
+                {
+                if (C != A) Console.WriteLine("Escaleno");
+                }
+                if (B == C) Console.WriteLine("Isósceles");
+            }
+
+            if (A == C)
+            {
+                if (B != C) Console.WriteLine("Isóceles");
+            }
+
+
+
+
+        }
+    }
+}
