971ef2f598b3d3b3717c6d341cb2b85d6e1adbd1
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 10:55:02 -0300

    Exercicio C#

---
+++ Atividade3-NúmerosIguais/Atividade3-NúmerosIguais/Atividade3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Atividade3_NúmerosIguais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Olá usuário, informe o primeiro número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Agora, informe o segundo número !");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            
+            if (n1 > n2)
+            {
+                Console.WriteLine("O maior número é o : " + n1);
+            }
+            else
+            {
+                if (n1 == n2)
+                {
+                    Console.WriteLine("NÚMEROS IGUAIS " + n1 + " e o " + n2);
+                }
+                else
+                {
+                    Console.WriteLine("O maior número é o :" + n2);
+                }
+            }
+        }
+    }
+}
