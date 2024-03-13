b8d4651d1e336d357f7934288e8e64159cd63cef
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 10:54:00 -0300

    Exercicio C#

---
+++ Atividade3Maças/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qntdMacas;
+
+            Console.WriteLine("Quantas maças foram compradas ?");
+            qntdMacas = Convert.ToInt32(Console.ReadLine());
+
+            if (qntdMacas < 12)
+            {
+                Console.WriteLine("O total a ser pago (R$ 0,30):" + (qntdMacas * 0.30));
+            }
+            else
+            {
+                Console.WriteLine("O total a ser pago (R$ 0,25):" + (qntdMacas * 0.25));
+            }
+
+
+            /******************ALTERNATIVA*******************/
+
+            if (qntdMacas < 12) Console.WriteLine("O total a ser pago é (R$ 0,30):" + (qntdMacas * 0.30));
+            if (qntdMacas > 12) Console.WriteLine("O total a ser pago é (R$ 0,25):" + (qntdMacas * 0.25));
+
+            Console.ReadKey();
+        }
+    }
+}
