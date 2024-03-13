b3f95145909249e11b0eb21559a8f1ec1d245319
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 10:57:12 -0300

    Exercicio C#

---
+++ Atividade4-NúmeroImparouPar/Atividade4-NúmeroImparouPar/Atividade4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Atividade4_NúmeroImparouPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+
+            Console.WriteLine("Olá usuário, informe um número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            if ((n1 / 2) == 0)
+            {
+                Console.WriteLine("O número :" + n1 + " é um número PAR");
+            }
+            else
+            {
+                Console.WriteLine("O número :" + n1 + " é um número IMPAR");
+            }
+
+            Console.ReadKey();
+
+            //Não deu certo professor, queria saber qual foi o erro, já que esse exercício é opcional//
+        }
+    }
+}
---
+++ Atividade5-Entre20E90/Atividade5-Entre20E90/Atividade5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Atividade5_Entre20E90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+
+            Console.WriteLine("Olá usuário, informe um número por favor !");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            if(n1 >= 20)
+            {
+                if (n1 <= 90)
+                {
+                    Console.WriteLine("Número correto");
+                }
+                else
+                {
+                    Console.WriteLine("Número completamente errado !");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Número errado !!"); 
+            }
+           
+        }
+    }
+}
