4428d1fbfb92a884b45cef8260d159eec82a6d03
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 10:49:54 -0300

    Exercicio C#

---
+++ Atividade2Numeros/Atividade2Numeros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Atividade2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("Informe o primeiro número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número !");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 > n2)
+            {
+                Console.WriteLine("Número maior é o :" + n1);
+            }
+            else
+            {
+                if (n1 == n2)
+                {
+                    Console.WriteLine("Números iguais :" + n1 + " e o " + n2);
+                }
+                else
+                {
+                    Console.WriteLine("O número maior é :" + n2);
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
