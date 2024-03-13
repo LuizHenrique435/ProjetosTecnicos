7b11617c5f516e20c7e3cf8292ebdaaa82471741
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 10:51:57 -0300

    Exercicio C#

---
+++ Atividade2-OrderDosNúmeros/Atividade2-OrderDosNúmeros/Atividade2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Atividade2_OrderDosNúmeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            int n3 = 0;
+            int n4aux = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Beep();
+
+            Console.WriteLine("Olá usuário, informe o primeiro número !");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Beep();
+
+            Console.WriteLine("Por favor, informe o segundo número !");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Beep();
+
+            Console.WriteLine("Agora finalmente, informe o último número !");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Beep();
+
+            if(n1 > n2)
+            {
+                n4aux = n1;
+                n1 = n2;
+                n2 = n4aux;
+            }
+
+            if (n1 > n3)
+            {
+                n4aux = n1;
+                n1 = n3;
+                n3 = n4aux;
+            }
+            if (n2 > n3)
+            {
+                n4aux = n2;
+                n2 = n3;
+                n3 = n4aux;
+            }
+
+            Console.WriteLine("Então, a ordem dos números em ordem crescente é: " + n1 + " , " + n2 + " , " + n3);
+
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
