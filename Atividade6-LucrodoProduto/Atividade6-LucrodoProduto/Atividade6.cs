5f3b82bae057408187739d759c6b9b7012d7fa81
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 11:00:20 -0300

    Exercicio C#

---
+++ Atividade6-LucrodoProduto/Atividade6-LucrodoProduto/Atividade6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Atividade6_LucrodoProduto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ValorProduto;
+
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Olá cliente, informe o valor do produto comprado !");
+            ValorProduto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Beep();
+
+            if(ValorProduto < 20.00)
+            {
+                Console.WriteLine("O valor de venda é igual a:" + (ValorProduto * 1.45));
+            }
+            else
+            {
+                if(ValorProduto >= 20.00)
+                {
+                    Console.WriteLine("Caso contrário o valor de venda é igual a:" +(ValorProduto * 1.30));
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Atividade7-AprovadoOuReprovado/Atividade7-AprovadoOuReprovado/Atividade7.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Atividade7_AprovadoOuReprovado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            double nota1, nota2, nota3;
+            int Faltas;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Beep();
+
+            Console.WriteLine("Olá estudante, por favor informe seu nome !!");
+            Nome = Console.ReadLine();
+
+            Console.Beep();
+
+            Console.WriteLine("Agora,por favor, informe a primeira nota !");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Beep();
+
+            Console.WriteLine("A segunda nota por favor !");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Beep();
+
+            Console.WriteLine("Finalmente informe a terceira e última nota !!");
+            nota3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Beep();
+
+            Console.WriteLine("Agora informe o número de faltas !!");
+            Faltas = Convert.ToInt32(Console.ReadLine());
+
+            Console.Beep();
+            Console.Beep();
+
+            Console.WriteLine("A média foi igual a: " + ((nota1 + nota2 + nota3) /3 ));
+            Console.WriteLine("O número de faltas que o estudante pode ter é: = 20");
+
+            Console.Beep();
+
+            if (Faltas > 20)
+            {
+                Console.WriteLine("REPROVADO !!!");
+            }
+            else
+            {
+                if((nota1+nota2+nota3)/3 >= 7.00)
+                {
+                    Console.WriteLine("APROVADO !!!");
+                }
+                else
+                {
+                    Console.WriteLine("REPROVADO !!!");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
