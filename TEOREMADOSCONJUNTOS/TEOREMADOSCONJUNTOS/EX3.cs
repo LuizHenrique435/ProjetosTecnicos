fdedc4ec3fd64d99aef27b361c96694049baf334
Luiz Henrique Souza Silva <141936881+LuizHenrique435@users.noreply.github.com>
2024-03-13 12:10:26 -0300

    Exercicio C#

---
+++ TEOREMADOSCONJUNTOS/TEOREMADOSCONJUNTOS/EX3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TEOREMADOSCONJUNTOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[10];
+            int[] vetor2 = new int[10];
+
+            for (int conta = 0; conta < 10; conta++)
+            {
+                Console.WriteLine("GRUPO 1");
+                Console.WriteLine("Informe um número !");
+                vetor1[conta] = Convert.ToInt32(Console.ReadLine());
+            }
+             for(int contadnv = 0; contadnv < 10; contadnv++)
+            {
+                Console.WriteLine("GRUPO 2");
+                Console.WriteLine("Informe um número !");
+                vetor2[contadnv] = Convert.ToInt32(Console.ReadLine());
+            }
+
+             for(int mostra = 0; mostra < 10; mostra++)
+            {
+                Console.WriteLine("A união desses dois conjuntos é igual a : " + (vetor1[mostra] + " e " + vetor2[mostra]));
+            }
+
+            for(int mostrar = 0; mostrar < 10; mostrar++)
+            {
+                Console.WriteLine("A diferenca dos conjuntos é igual a : " + (vetor1[mostrar] != vetor2[mostrar]));
+            }
+
+            for(int S = 0; S < 10; S++)
+            {
+                Console.WriteLine("A intersecção dos conjuntos é igual a : " + (vetor1[S] == vetor2[S]));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ZuBank/ZuBank/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ZuBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string correntista;
+            int mesesInvestidos;
+            double valorInvestido;
+            double valorTotal;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("===============");
+            Console.WriteLine("====ZUBANK=====");
+            Console.WriteLine("===============");
+
+            Console.WriteLine("Olá, bom dia, bem vindo ao ZuBank");
+            Console.WriteLine("Por favor, informe o nome do correntista !!");
+            correntista = Console.ReadLine();
+
+            Console.WriteLine("Olá Sr." + correntista + "por favor informe o total de meses que serão investidos !!");
+            mesesInvestidos = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Então Sr" + correntista + "o lucro por cada mes investido será de 0,03");
+            Console.WriteLine("Por favor, informe o valor que será investido !!");
+            valorInvestido = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("O lucro será de :" + (valorTotal = (1 + 0.03) * mesesInvestidos * valorInvestido));
+
+        }
+    }
+}
